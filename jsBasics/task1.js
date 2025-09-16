0ac6ac1dda818f4fd2c98aabeada2ceac52cdf7c
Olena Salnikova <nameless.hire5@gmail.com>
2025-09-15 20:23:06 -0400

    HW 4.1. JavaScript primitive data types

---
+++ jsBasics/task1.js
@@ -0,0 +1,13 @@
+// Declaration of variables with primitive data types
+const myString = "Hello, JavaScript!"; // string
+const myNumber = 22;                   // number
+const myBoolean = true;                // boolean
+const myNull = null;                   // null
+const myUndefined = undefined;         // undefined
+
+// Output to console
+console.log("String:", myString);
+console.log("Number:", myNumber);
+console.log("Boolean:", myBoolean);
+console.log("Null:", myNull);
+console.log("Undefined:", myUndefined);
---
+++ jsBasics/task3.js
@@ -0,0 +1,9 @@
+// Declare a variable for age
+const age = 27;
+
+// Boolean variable to check if the person is an adult (18 or older)
+const isAdult = age >= 18;
+
+// Output variables to the console
+console.log("Age:", age);
+console.log("Is adult:", isAdult);
