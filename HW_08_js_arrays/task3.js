85836590b64aa5699659f160dfa792824c001ada
Olena Salnikova <nameless.hire5@gmail.com>
2025-09-28 18:09:56 -0400

    HW 8.1. Arrays in JavaScript

---
+++ HW_08_js_arrays/task3.js
@@ -0,0 +1,5 @@
+const numbers = [10, 20, 30, 40, 50];
+const sumOfNumbers = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+// Output the sum of the numbers
+console.log('Sum of Numbers:', sumOfNumbers);
---
+++ HW_08_js_arrays/task5.js
@@ -0,0 +1,6 @@
+const firstArray = [1, 2, 3];
+const secondArray = [4, 5, 6];
+const combinedArray = firstArray.concat(secondArray);
+
+// Output the combined array
+console.log('Combined Array:', combinedArray);
