cddb74377913836ab96928dea7ee8a811f46df2e
Olena Salnikova <nameless.hire5@gmail.com>
2025-10-14 17:19:00 -0400

    Renamed function according to request

--- HW_11_js_promises/task1.js
+++ HW_11_js_promises/task1.js
@@ -1,9 +1,9 @@
-function printMessage(message, delay) {
+function printMessageWithDelay(message, delay) {
     return setTimeout(() => console.log(message), delay); // Using setTimeout to simulate a delay
 }
 
-printMessage("Hello after 1 second", 1000);
-printMessage("Hello after 2 seconds", 2000);
-printMessage("Hello after 3 seconds", 3000);
-printMessage("Hello after 4 seconds", 4000);
-printMessage("Hello after 5 seconds", 5000);
+printMessageWithDelay("Hello after 1 second", 1000);
+printMessageWithDelay("Hello after 2 seconds", 2000);
+printMessageWithDelay("Hello after 3 seconds", 3000);
+printMessageWithDelay("Hello after 4 seconds", 4000);
+printMessageWithDelay("Hello after 5 seconds", 5000);
